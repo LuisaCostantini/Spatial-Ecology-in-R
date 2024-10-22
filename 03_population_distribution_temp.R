96f83a205ed9dbc9935e76a47f2dae212d6ca8c4
LuisaCostantini <126901462+LuisaCostantini@users.noreply.github.com>
2024-10-22 17:03:59 +0200

    Update 03_population_distribution_temp.R

--- 03_population_distribution_temp.R
+++ 03_population_distribution_temp.R
@@ -1,22 +1,66 @@
+#03 population distribution on R 
+
+# Why populations disperse over the landscape in a certain manner?
+
+library(sdm)
+library(terra)
+
+file <- system.file("external/species.shp", package="sdm")
+
+rana <- vect(file)
+rana$Occurrence
+
+plot(rana)
+
+# Selecting presences
+pres <- rana[rana$Occurrence==1,]
+plot(pres)
+
+# exercise: select absence and call them abse
+abse <- rana[rana$Occurrence==0,]
+plot(abse)
+
+# exrecise: plot presences and absences, one beside the other
+par(mfrow=c(1,2))
+plot(pres)
+plot(abse)
+
+# your new friend in case of graphical nulling:
+dev.off()
+
+# exercise: plot pres and abse altogether with two different colours
+plot(pres, col="dark blue")
+points(abse, col="light blue")
+
+# predictors: environmental variables
+# file <- system.file("external/species.shp", package="sdm")
+# rana <- vect(file)
+
+# elevation predictor
+elev <- system.file("external/elevation.asc", package="sdm") 
+elevmap <- rast(elev) # from terra package
+plot(elevmap)
+points(pres, cex=.5)
+
 # temperature predictor
 temp <- system.file("external/temperature.asc", package="sdm") 
 tempmap <- rast(temp) # from terra package
 plot(tempmap)
 points(pres, cex=.5)
 
-# exrcise: do the same with vegetation cover
+# Exercise: do the same with vegetation cover
 vege <- system.file("external/vegetation.asc", package="sdm") 
 vegemap <- rast(vege) # from terra package
 plot(vegemap)
 points(pres, cex=.5)
 
-# exrcise: do the same with vegetation cover
+# Exercise: do the same with precipitation cover
 prec <- system.file("external/precipitation.asc", package="sdm") 
 precmap <- rast(prec) # from terra package
 plot(precmap)
 points(pres, cex=.5)
 
-# final multiframe
+# Final multiframe
 
 par(mfrow=c(2,2))
 
@@ -35,3 +79,4 @@ points(pres, cex=.5)
 # prec
 plot(precmap)
 points(pres, cex=.5)
+
